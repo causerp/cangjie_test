4f634c68718f748374fbd72ffcfe02eadf798f47
Tadddy <yinkejie@huawei.com>
2025-11-25 15:18:56 +0800

    fix: active state const analysis bugfix
    Signed-off-by: Tadddy <yinkejie@huawei.com>

---
+++ testsuites/LLT/compiler/CHIROpt/DeadCodeElimination/UselessFunction/DCECFuncHeader.h
@@ -0,0 +1,14 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+
+#ifndef DCE_CFUNC_HEADER_H
+#define DCE_CFUNC_HEADER_H
+
+#include <stdint.h>
+
+void charger(uint8_t buf[]);
+
+#endif
