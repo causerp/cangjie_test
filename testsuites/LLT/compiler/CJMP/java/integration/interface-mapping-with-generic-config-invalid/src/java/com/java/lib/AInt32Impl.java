edb955901b7c8ba4f748ed0bdc113ca930c3079c
jiadiliu <liujiadi3@huawei.com>
2025-11-27 09:06:16 +0000

    feat: support java->cj interface generic type
    Signed-off-by: jiadiliu <liujiadi3@huawei.com>

---
+++ testsuites/LLT/compiler/CJMP/java/integration/interface-mapping-with-generic-config-invalid/src/java/com/java/lib/AInt32Impl.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This source file is part of the Cangjie project, licensed under Apache-2.0
+ * with Runtime Library Exception.
+ *
+ * See https://cangjie-lang.cn/pages/LICENSE for license information.
+ */
+ 
+package com.java.lib;
+ 
+import cj.*;
+ 
+public class AInt32Impl implements AInt32 {
+    @Override
+    public void goo(int v) {
+        System.out.print("Java: AInt32Impl goo()!");
+        System.out.print(v);
+        System.out.println();
+    }
+
+    @Override
+    public int doo(int v) {
+        System.out.println("Java: AInt32Impl doo()!");
+        return v*2;
+    }
+}
