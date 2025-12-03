16aa37eac2ab2c462eaab0dfbafd90470ce933b7
jiangjiji <jiangjiji@huawei.com>
2025-12-03 17:53:27 +0800

    fix: fix enum dependecy bug
    Signed-off-by: Jiangjiji <jiangjiji@huawei.com>

---
+++ testsuites/LLT/compiler/CJMP/objc/integration/cjmapping_enum_dependencies/impl_enum_0021/src/objc/main.m
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This source file is part of the Cangjie project, licensed under Apache-2.0
+ * with Runtime Library Exception.
+ *
+ * See https://cangjie-lang.cn/pages/LICENSE for license information.
+ */
+
+#import "A1.h"
+#import "A2.h"
+#import <Foundation/Foundation.h>
+
+int main(int argc, char** argv) {
+    @autoreleasepool {
+        A1* a1 = [A1 E11];
+        A2* a2 = [A2 E21];
+        [a2 f: a1];
+    }
+    return 0;
+}
