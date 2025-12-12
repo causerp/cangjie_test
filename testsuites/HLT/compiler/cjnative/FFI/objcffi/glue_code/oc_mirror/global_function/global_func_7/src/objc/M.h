048ef4813e5768c2049a05b22b73190077030629
Al_bkt_for_brine <wubenteng1@huawei.com>
2025-12-12 11:03:37 +0800

    test: add oc interop test cases
    Signed-off-by: Al_bkt_for_brine <wubenteng1@huawei.com>

---
+++ testsuites/HLT/compiler/cjnative/FFI/objcffi/glue_code/oc_mirror/global_function/global_func_7/src/objc/M.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This source file is part of the Cangjie project, licensed under Apache-2.0
+ * with Runtime Library Exception.
+ * 
+ * See https://cangjie-lang.cn/pages/LICENSE for license information.
+ */
+
+#import "P.h"
+#import <Foundation/Foundation.h>
+struct S  {
+int x;
+int y;
+};
+
+void f(int64_t* a);
+
+@interface M :NSObject <P> {
+    struct S* v0;
+    NSObject<P> *v1;
+    M* v2;
+    id<P> v3;
+    @public
+    void (*fptr)(int64_t*);
+}
+- (id)init;
+- (void)mFoo;
+- (struct S*)mFoo1:(struct S*)p0;
+- (NSObject<P> *)mFoo2:(NSObject<P> *)p0;
+- (M*)mFoo3:(M*)p0;
+- (id<P>)mFoo4:(id<P>)p0;
+- (M**)mFoo5:(M**)p0;
+
+@property (nonatomic, readwrite) struct S* p0;
+@property (nonatomic, readwrite) NSObject<P> * p1;
+@property (nonatomic, readwrite) M* p2;
+@property (nonatomic, readwrite) id<P> p3;
+@end
---
+++ testsuites/HLT/compiler/cjnative/FFI/objcffi/glue_code/oc_mirror/global_function/global_func_7/src/objc/main.m
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This source file is part of the Cangjie project, licensed under Apache-2.0
+ * with Runtime Library Exception.
+ * 
+ * See https://cangjie-lang.cn/pages/LICENSE for license information.
+ */
+
+#import "A.h"
+#import "G.h"
+#import <Foundation/Foundation.h>
+int main(int argc, char** argv) {
+@autoreleasepool {
+A* a= [[A alloc] init];
+[a foo];
+id<P> result = global_func(a, a, f);
+[result pFoo];
+}
+
+return 0;
+}
