d1e76ca832b4b865cbe7c2284d37d2adc7bc589c
belyaev-mikhail <belyaev.mikhail@huawei.com>
2025-12-16 18:04:24 +0300

    feat: objcblock construction from function
    Signed-off-by: belyaev-mikhail <belyaev.mikhail@huawei.com>

---
+++ testsuites/LLT/compiler/CJMP/objc/integration/objc_block_cangjie/src/objc/main.m
@@ -0,0 +1,36 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+
+#import <Foundation/Foundation.h>
+#import "Cangjie.h"
+#import <dlfcn.h>
+#import <stdlib.h>
+#import "M.h"
+
+static void* CJWorldDLHandle = NULL;
+static struct RuntimeParam defaultCJRuntimeParams = {0};
+
+extern int cangjieMain();
+
+int main(int argc, char** argv) {
+    if (InitCJRuntime(&defaultCJRuntimeParams) != E_OK) {
+        NSLog(@"ERROR: Failed to initialize Cangjie runtime");
+        exit(1);
+    }
+    if (LoadCJLibraryWithInit("libcjworld.dylib") != E_OK) {
+        NSLog(@"ERROR: Failed to init cjworld library ");
+        exit(1);
+    }
+    if ((CJWorldDLHandle = dlopen("libcjworld.dylib", RTLD_LAZY)) == NULL) {
+        NSLog(@"ERROR: Failed to open cjworld library ");
+        NSLog(@"%s", dlerror());
+        exit(1);
+    }
+    @autoreleasepool {
+        return cangjieMain();
+    }
+    return 0;
+}
