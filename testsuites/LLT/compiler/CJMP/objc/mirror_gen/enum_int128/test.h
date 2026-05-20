9b704180aafcbfedcebc129231f7ad1e4c702e0a
Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>
2026-05-20 17:17:32 +0700

    fix(objcinteropgen): rebasing on !160
    Signed-off-by: Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>

--- testsuites/LLT/compiler/CJMP/objc/mirror_gen/enum_int128/test.h
+++ testsuites/LLT/compiler/CJMP/objc/mirror_gen/enum_int128/test.h
@@ -6,4 +6,4 @@
  * See https://cangjie-lang.cn/pages/LICENSE for license information.
  */
 
-enum M : __int128 { a, b = 1, c = 0xffffffffffffffff, d };
+enum M : unsigned __int128 { a, b = 1, c = 0xffffffffffffffff, d };
