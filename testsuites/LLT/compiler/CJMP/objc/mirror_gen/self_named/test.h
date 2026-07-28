38c44e242b2661b4b1da7d35323f025a11792b93
Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>
2026-07-28 15:21:16 +0700

    fix(objcinteropgen): comment out more cases of object/type name clash (!176)
    Signed-off-by: Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>

---
+++ testsuites/LLT/compiler/CJMP/objc/mirror_gen/self_named/test.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ * This source file is part of the Cangjie project, licensed under Apache-2.0
+ * with Runtime Library Exception.
+ *
+ * See https://cangjie-lang.cn/pages/LICENSE for license information.
+ */
+
+typedef int T1;
+typedef int T2;
+typedef int T3;
+typedef int T4;
+typedef int T5;
+typedef int T6;
+typedef int T7;
+
+struct S {
+    T1 T1;
+    T2* T2;
+};
+
+@interface M {
+    struct S S;
+    T1* T1;
+}
+- (T2)T2;
+- (T3*)T3;
+- (void)foo:(T3)T3;
+- (void)T4:(T4)x;
+- (void)T5:(T5*)x;
+@property T6 T6;
+@property T7* T7;
+@end
