3af4887286b36ae7b009cb8369ec29b03a3d1d36
dmitriy sokolov <sokolov.dmitriy@huawei-partners.com>
2026-08-04 18:59:31 +0300

    fix: ffi java and objc remove cjmapping & fix java generator accessors
    Signed-off-by: dmitriy sokolov <sokolov.dmitriy@huawei-partners.com>

--- testsuites/LLT/compiler/CJMP/objc/integration/cj-mapping-generic-interface/src/objc/GenericInterfaceFloat64Impl.h
+++
@@ -1,4 +0,0 @@
-#import "GenericInterfaceFloat64.h"
-
-@interface GenericInterfaceFloat64Impl : NSObject <GenericInterfaceFloat64>
-@end
--- testsuites/LLT/compiler/CJMP/objc/integration/cjmapping_open_class/src/objc/BB.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <stddef.h>
-#import "Base.h"
-
-@interface BB : Base
-- (void)hello;
-@end
