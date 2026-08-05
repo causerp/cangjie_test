4868c7de98af9d3d4db7c5183d60be658cd45c5e
Anastasia Shadrina <shadrina.anastasia1@huawei.com>
2026-08-05 17:54:39 +0700

    fix(objc): remove ObjCOptional from override members
    Signed-off-by: Anastasia Shadrina <shadrina.anastasia1@huawei.com>

---
+++ testsuites/LLT/compiler/CJMP/objc/mirror_gen/optional_on_overrides/test.h
@@ -0,0 +1,19 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ * This source file is part of the Cangjie project, licensed under Apache-2.0
+ * with Runtime Library Exception.
+ *
+ * See https://cangjie-lang.cn/pages/LICENSE for license information.
+ */
+
+@protocol P
+@optional
+- (void)method;
+- (void)method2;
+@end
+
+@protocol M <P>
+@optional
+- (void)method;
+- (void)method2;
+@end
