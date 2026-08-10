eb28849a588015eae544986a9c51426f05de007e
Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>
2026-08-10 13:12:06 +0700

    fix(objcinteropgen): fix resolving enum clashes
    Signed-off-by: Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>

--- testsuites/LLT/compiler/CJMP/objc/mirror_gen/tagged_non_tagged_clash/test.h
+++ testsuites/LLT/compiler/CJMP/objc/mirror_gen/tagged_non_tagged_clash/test.h
@@ -6,5 +6,11 @@
  * See https://cangjie-lang.cn/pages/LICENSE for license information.
  */
 
-struct M {};
-typedef int M;
+struct M1 {};
+typedef int M1;
+
+union M2 {};
+typedef int M2;
+
+enum M3 : int { a };
+typedef int M3;
