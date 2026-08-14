f46eebed401427cafe48b46bbebc9ae71eceb75b
Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>
2026-08-14 17:19:57 +0700

    fix(objcinteropgen): resolve parameter name clashes
    Signed-off-by: Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>

---
+++ testsuites/LLT/compiler/CJMP/objc/mirror_gen/same-name-params/test.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ * This source file is part of the Cangjie project, licensed under Apache-2.0
+ * with Runtime Library Exception.
+ *
+ * See https://cangjie-lang.cn/pages/LICENSE for license information.
+ */
+
+@interface M
+- (void)foo:(int)x x:(int)x;
+- (void)foo:(int)x:(int)x;
+- (void)foo:(int)x2:(int)x:(int)x;
+@end
+
+void M1(int);
+void M2(int, int x);
+void M3(int x, int);
+void M4(int, int);
+void M5(int _2, int, int);
