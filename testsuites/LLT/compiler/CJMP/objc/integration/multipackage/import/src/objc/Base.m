3ebbc6f01a9b831ebd6b42c363a7f10e3f4272de
belyaev-mikhail <belyaev.mikhail@huawei.com>
2026-09-22 13:09:52 +0300

    fix: allow java interop callsite desugaring in packages without mirrors or impls
    Signed-off-by: belyaev-mikhail <belyaev.mikhail@huawei.com>

---
+++ testsuites/LLT/compiler/CJMP/objc/integration/multipackage/import/src/objc/Base.m
@@ -0,0 +1,19 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+
+#import "Base.h"
+
+@implementation Base
+- (id)init {
+    return self;
+}
+- (int64_t)foo {
+    return 1;
+}
+- (id)getSelf {
+    return self;
+}
+@end
---
+++ testsuites/LLT/compiler/CJMP/objc/integration/multipackage/import/src/objc/main.m
@@ -0,0 +1,21 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+
+#import <Foundation/Foundation.h>
+#import <stdlib.h>
+#import <assert.h>
+#import "Base.h"
+// Interoplib objc common code
+extern bool initCJRuntime(const char*);
+
+extern int cangjieMain();
+
+int main(int argc, char** argv) {
+    if (!initCJRuntime("libcjworld.dylib")) {
+        exit(1);
+    }
+    return cangjieMain();
+}
